d795d1dfa414f5b4679b04032ece1c43a226d858
jgilliland <jgilliland@apqc.org>
2013-07-24 17:10:02 -0500

    Issue #1162688 by drupalninja99, MiSc | jmcsweet: Port to Drupal 7.

--- pardot-js.tpl.php
+++ pardot-js.tpl.php
@@ -1,15 +1,15 @@
 piAId = "<?php print $pardot_a_id; ?>";
 piCId = "<?php print $pardot_c_id; ?>";
-<?php if ($score):?>
+<?php if ($score): ?>
 piPoints = "<?php print $score; ?>";
 <?php endif; ?>
 
 (function() {
-	function async_load(){
-		var s = document.createElement('script'); s.type = 'text/javascript';
-		s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
-		var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
-	}
-	if(window.attachEvent) { window.attachEvent('onload', async_load); }
-	else { window.addEventListener('load', async_load, false); }
+function async_load(){
+  var s = document.createElement('script'); s.type = 'text/javascript';
+  s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+  var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+}
+if(window.attachEvent) { window.attachEvent('onload', async_load); }
+else { window.addEventListener('load', async_load, false); }
 })();
