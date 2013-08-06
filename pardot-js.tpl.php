9c90a3417e531a6dc3e812b71084ecd45e2216ea
Jay Callicott <jaykali@42248.no-reply.drupal.org>
2013-08-06 10:01:49 -0500

    Adding better screenshot example

--- pardot-js.tpl.php
+++ pardot-js.tpl.php
@@ -1,15 +1,21 @@
-piAId = "<?php print $pardot_a_id; ?>";
-piCId = "<?php print $pardot_c_id; ?>";
+<?php
+/**
+ * @file
+ * Render Pardot JavaScript and parameters
+ */
+?>
+piAId = "<?php print check_plain($pardot_a_id); ?>";
+piCId = "<?php print check_plain($pardot_c_id); ?>";
 <?php if ($score): ?>
-piPoints = "<?php print $score; ?>";
+  piPoints = "<?php print check_plain($score); ?>";
 <?php endif; ?>
 
 (function() {
-function async_load(){
-  var s = document.createElement('script'); s.type = 'text/javascript';
-  s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
-  var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
-}
-if(window.attachEvent) { window.attachEvent('onload', async_load); }
-else { window.addEventListener('load', async_load, false); }
+  function async_load(){
+    var s = document.createElement('script'); s.type = 'text/javascript';
+    s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+    var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+  }
+  if(window.attachEvent) { window.attachEvent('onload', async_load); }
+  else { window.addEventListener('load', async_load, false); }
 })();
